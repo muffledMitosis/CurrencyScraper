e4ca70b4f21fec8aee16f25ab9d772982e8ddfaf
Meth Munindradasa <methsndr@gmail.com>
2023-06-07 20:59:58 +0530

    raw data format standard and improve readability

--- src/Scrapers.py
+++ src/Scrapers.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+from dataclasses import dataclass
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -8,13 +10,25 @@ from selenium.webdriver.common.by import By
 # Path to ChromeDriver executable
 webdriver_path = "/home/meth/chromedriver/stable/chromedriver"
 
+@dataclass
+class SeleniumConfig:
+	service: Service
+	options: webdriver.ChromeOptions
+
 class CurrencyScraper:
-	def __init__(self, url=""):
+	def __init__(self, url="", readsite=True):
 		self.url = url
 		self.currencyList = []
+
+		# raw data format -> [iso4217, buying, selling]
+		self.raw_data = []
+
 		self.resp = None
 		self.soup = None
-		self.__readSite()					# TODO: Implement caching. Dont wanna overload servers
+		
+		# Don't send request if using selenium
+		if readsite:
+			self.__readSite()					# TODO: Implement caching. Dont wanna overload servers
 	
 	def __readSite(self):
 		self.resp = requests.get(self.url)
@@ -26,7 +40,7 @@ class CurrencyScraper:
 			print(row)
 
 class CommBankScraper(CurrencyScraper):
-	def __init__(self, url=""):
+	def __init__(self):
 		super().__init__("https://www.combank.lk/rates-tariff")
 
 	def scrape(self):
@@ -37,15 +51,13 @@ class CommBankScraper(CurrencyScraper):
 		rows = table.find_all("tr")
 		rows = rows[3:]						# Remove garbage rows
 
-		self.raw_data = []
-
 		for row in rows:
 			cells = row.find_all("td")
 			first_three_columns = [cell.text.strip() for cell in cells[:3]]
 			self.raw_data.append(first_three_columns)
 
 class SeylanBankScraper(CurrencyScraper):
-	def __init__(self, url=""):
+	def __init__(self):
 		super().__init__("https://www.seylan.lk/exchange-rates")
 
 	def scrape(self):
@@ -55,33 +67,43 @@ class SeylanBankScraper(CurrencyScraper):
 		rows = table.find_all("tr")
 		rows = rows[2:]						# Remove garbage rows
 
-		self.raw_data = []
-
 		for row in rows:
 			cells = row.find_all("td")
 			first_three = [cell.text.strip() for cell in cells[1:4]]
 			self.raw_data.append(first_three)
 
 class SampathBankScraper(CurrencyScraper):
-	def __init__(self, url=""):
-		super().__init__("https://www.sampath.lk/rates-and-charges?activeTab=exchange-rates")
+	def __init__(self):
+		super().__init__("https://www.sampath.lk/rates-and-charges?activeTab=exchange-rates", readsite=False)
+		# TODO: Implement caching for selenium and nav
 		self.__initialize_selenium()
 		
 		# Navigate to webpage
-		self.s_driver.get(self.url)
+		self.selenium_driver.get(self.url)
 
 	def __initialize_selenium(self):
-		self.s_service = Service(webdriver_path)
-		self.s_options = webdriver.ChromeOptions()
-		self.s_options.add_argument("--headless")
-		self.s_driver = webdriver.Chrome(service=self.s_service, options=self.s_options)
+		self.selenium_config = SeleniumConfig(
+			Service(webdriver_path),
+			webdriver.ChromeOptions(),
+		)
+
+		self.selenium_config.options.add_argument("--headless")
+
+		self.selenium_driver = webdriver.Chrome(service=self.selenium_config.service,
+		    options=self.selenium_config.options
+		)
 	
 	def scrape(self):
-		the_table = self.s_driver.find_element(By.ID, "__BVID__413")
+		the_table = self.selenium_driver.find_element(By.ID, "__BVID__413")
 		rows = the_table.find_elements(By.TAG_NAME, "tbody")
 
 		for row in rows:
+			# Get the actuall data
 			cell_row = row.find_element(By.TAG_NAME, "tr")
 			cells = cell_row.find_elements(By.TAG_NAME, "td")
+			
+			# Drop what we don't need
 			text = [cell.get_attribute("innerHTML").strip() for cell in cells]
-			print(text)
+			text = [text[0], text[2], text[4]]
+
+			self.raw_data.append(text)
